11d60fb5345a9cf02f03b4adf30fa4b1a0d4be91
Marina2609 <78434449+Marina2609@users.noreply.github.com>
2021-11-06 02:45:59 +0300

    feat: added code to 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,30 @@
+const fileSystem = require('fs');
+const path = require('path');
+const source = path.join(__dirname, 'files');
 
+fileSystem.readdir(source, {withFileTypes: true}, (error, files) => {
+    if (error) {
+        console.log(error);
+    }
+    fileSystem.mkdir('04-copy-directory/files-copy', {recursive: true}, error => {
+        if (error) {
+            console.log(error);
+        }
+    });
+    
+    const destination = path.join(__dirname, 'files-copy');
+
+    for (let file of files) {
+        fileSystem.readFile(`${source}/${file.name}`, (error, data) => {
+            if (error) {
+                console.log(error);
+            }
+            fileSystem.writeFile(`${destination}/${file.name}`, data, error => {
+                if (error) {
+                    console.log(error);
+                }
+                console.log(`File ${file.name} recorded`);
+            });
+        });
+    }
+});
